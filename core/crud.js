89aa5a78dedb917c1fff2fb80413a62d3f2d5a75
francojeferson <jefersonfranco@pm.me>
2025-05-29 15:02:19 -0300

    feat: add basic CRUD operations with file-based storage
    - implement create function to write content to db file
    - add database file with initial 'Hello World' content
    - include logging and simulation code for testing

--- core/crud.js
+++ core/crud.js
@@ -1,6 +1,10 @@
+const fs = require('fs')
+const DB_FILE_PATH = './core/db'
 console.log('[CRUD]')
 
 function create(content) {
+  // save content on system
+  fs.writeFileSync(DB_FILE_PATH, content)
   return content
 }
 
