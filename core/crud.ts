a321513318d908db639c044ae6a7cf861053bfa4
francojeferson <jefersonfranco@pm.me>
2025-05-29 17:02:15 -0300

    feat: initialize TypeScript CRUD application with file-based storage
    - Add core CRUD module with create function using fs.writeFileSync
    - Configure TypeScript with CommonJS module system and ES2016 target
    - Set up development environment with ts-node and nodemon
    - Add npm scripts for running and watching CRUD operations
    - Initialize file-based database storage in core/db

--- core/crud.ts
+++ core/crud.ts
@@ -1,12 +1,13 @@
-const fs = require('fs')
+import fs from 'fs' //ES6
+// const fs = require('fs') //CommonJS
 const DB_FILE_PATH = './core/db'
 console.log('[CRUD]')
 
-function create(content) {
+function create(content: string) {
   // save content on system
   fs.writeFileSync(DB_FILE_PATH, content)
   return content
 }
 
 // [SIMULATION]
-console.log(create('testing nodemon'))
+console.log(create('testing ts-node'))
